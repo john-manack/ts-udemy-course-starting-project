73bbb9e7359ae89a1f285b6b44ca637e24cd43fe
John Manack <john.manack@semanticbits.com>
2021-09-27 09:11:41 -0400

    through 2.17

--- app.ts
+++ app.ts
@@ -1,18 +1,9 @@
-const add = (number1: number, number2: number, showResult: boolean, phrase: string) => {
-    console.log(typeof number1)
-    const result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result)
-    } else {
-        return number1 + number2;
-    }
-}
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: 'Maximilian',
+    age: 30
+};
 
-const n1 = 5;
-const n2 = 4.5;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-const result = add(n1, n2, printResult, resultPhrase);
-
-console.log(`The result is ${result}`);
+console.log(person.name);
