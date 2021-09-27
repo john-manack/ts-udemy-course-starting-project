bf2cadf2623fb8fb5589578364fccab2619f2017
John Manack <john.manack@semanticbits.com>
2021-09-27 08:52:24 -0400

    through 2.14

--- app.ts
+++ app.ts
@@ -1,10 +1,18 @@
-const add = (number1: number, number2: number) => {
-    return number1 + number2
+const add = (number1: number, number2: number, showResult: boolean, phrase: string) => {
+    console.log(typeof number1)
+    const result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result)
+    } else {
+        return number1 + number2;
+    }
 }
 
 const n1 = 5;
 const n2 = 4.5;
+const printResult = true;
+const resultPhrase = 'Result is: ';
 
-const result = add(n1, n2);
+const result = add(n1, n2, printResult, resultPhrase);
 
 console.log(`The result is ${result}`);
