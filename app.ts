1612d4a5d695a68a7731a4748ca6c03ee160b010
John Manack <john.manack@semanticbits.com>
2021-09-27 08:31:48 -0400

    through 2.12

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log('Time to get started...')
+const add = (number1: number, number2: number) => {
+    return number1 + number2
+}
+
+const n1 = 5;
+const n2 = 4.5;
+
+const result = add(n1, n2);
+
+console.log(`The result is ${result}`);
